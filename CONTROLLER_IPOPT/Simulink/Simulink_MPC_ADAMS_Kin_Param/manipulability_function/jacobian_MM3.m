13d97f7eaf3fe0a374ebed372b106935f81fdf5f
Raffaele Colombo <colombraf@gmail.com>
2019-02-11 21:48:27 -0800

    Auto stash before merge of "devRaffo-CODE" and "origin/develop"

--- CONTROLLER_IPOPT/Simulink/Simulink_MPC_ADAMS_Kin_Param/manipulability_function/jacobian_MM3.m
+++ CONTROLLER_IPOPT/Simulink/Simulink_MPC_ADAMS_Kin_Param/manipulability_function/jacobian_MM3.m
@@ -43,15 +43,9 @@ Jp_nh = [Jp(:,1:3)*G , Jp(:,4:end)];
 Jo_nh = [Jo(:,1:3)*G , Jo(:,4:end)];
 
 J_ee = [Jp_nh;Jo_nh];
-J_ee = J_ee(1:3,3:end);
-% J_ee = simplify(J_ee);
+J_ee = simplify(J_ee(:,3:end));
 
-man=det(J_ee*J_ee.');
-% man=simplify(man);
-man_f = matlabFunction(man,'File','myfile.m');
+% man=det(J_ee*J_ee.');
 
-tstart=tic;
+% man_f = matlabFunction(man,'File','man_f.m');
 
-[aa bb cc]=FK([1 2 3 4 5 6 7 8 9]);
-
-tel=toc(tstart);
